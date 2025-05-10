1b92bcf706572185b139913c3a9e97fc862767cb
Andrés Moreno <andres.moreno.barbosa@gmail.com>
2025-05-10 14:06:25 -0500

    Fix: Changed requires to gymnasium

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(name='gym_Rubiks_Cube',
       version='0.0.1',
-      install_requires=['gym', 'termcolor'],
-      url="https://github.com/RobinChiu/gym-Rubiks-Cube",
+      install_requires=['gymnasium', 'termcolor'],
+      url="https://github.com/andresmore/gym-Rubiks-Cube",
       packages=find_packages()
 )  
