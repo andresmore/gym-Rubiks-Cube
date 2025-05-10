24111096a76d809ef8bd9ebf01e63b4ba01e52f3
Andrés Moreno <andres.moreno.barbosa@gmail.com>
2025-05-10 15:35:40 -0500

    Fix: new api step setup

--- gym_Rubiks_Cube/__init__.py
+++ gym_Rubiks_Cube/__init__.py
@@ -3,10 +3,13 @@ from gym.envs.registration import register
 register(
     id='RubiksCube-v0',
     entry_point='gym_Rubiks_Cube.envs:RubiksCubeEnv',
+    new_step_api=True
+
 )
 
 register(
     id='RubiksCube2x2-v0',
     entry_point='gym_Rubiks_Cube.envs:RubiksCubeEnv',
     kwargs={'order_num' : 2},
+    new_step_api=True
 )
