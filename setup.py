337d52bd21a1131227485c74344c0986f92958ef
Andrés Moreno <andres.moreno.barbosa@gmail.com>
2025-05-10 15:34:31 -0500

    Fix: new api step setup

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ setup(name='gym_Rubiks_Cube',
       version='0.0.1',
       install_requires=['gym', 'termcolor'],
       url="https://github.com/andresmore/gym-Rubiks-Cube",
+      new_step_api=True,
       packages=find_packages()
 )  
